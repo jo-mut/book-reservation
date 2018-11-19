f3b056d7e6017cd3be58aa0af77d44ab48f79859
jo-ngei <johnngei2@gmail.com>
2018-11-19 16:30:34 +0300

    created the basic template for a restaurant booking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, NavigationEnd } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/restaurant', pathMatch: 'full'},
+  {path: 'restaurant', component: RestaurantComponent},
+  {path: '**', component: PageNotFoundComponent}
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [PageNotFoundComponent, RestaurantComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-
+import { AppRoutingModule, routingComponents} from './app-routing.module';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule,
+  MatInputModule, MatRadioModule, MatSelectModule, MatMenuModule,
+  MatSidenavModule, MatToolbarModule, MatCardModule, MatDividerModule,
+  MatExpansionModule, MatGridListModule, MatListModule, MatStepperModule,
+  MatTabsModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule,
+  MatProgressBarModule, MatDialogModule, MatSnackBarModule, MatTooltipModule,
+  MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ServicesComponent } from './services/services.component';
+import { ChefsComponent } from './chefs/chefs.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { FooterComponent } from './footer/footer.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { LandingComponent } from './landing/landing.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    NavBarComponent,
+    ServicesComponent,
+    ChefsComponent,
+    ReviewsComponent,
+    FooterComponent,
+    RestaurantComponent,
+    LandingComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule, MatCheckboxModule, MatFormFieldModule,
+    MatInputModule, MatRadioModule, MatSelectModule, MatMenuModule,
+    MatSidenavModule, MatToolbarModule, MatCardModule, MatDividerModule,
+    MatExpansionModule, MatGridListModule, MatListModule, MatStepperModule,
+    MatTabsModule, MatChipsModule, MatIconModule, MatProgressSpinnerModule,
+    MatProgressBarModule, MatDialogModule, MatSnackBarModule, MatTooltipModule,
+    MatPaginatorModule, MatSortModule, MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
